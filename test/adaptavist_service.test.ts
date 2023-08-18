56a23b5bed79cf3cf2f1c90af06faff12d87d625
peken97 <24995925+peken97@users.noreply.github.com>
2023-08-18 16:42:24 -0400

    Initial commit

---
+++ test/adaptavist_service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AdaptavistService from '../lib/adaptavist_service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adaptavist_service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AdaptavistService.AdaptavistServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
