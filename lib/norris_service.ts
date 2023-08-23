4e621dabc7a59dd0533ea94384e0cc9ef7b4f3ce
peken97 <24995925+peken97@users.noreply.github.com>
2023-08-23 18:30:01 -0400

    added environment variable and lambda layers for the chuck norris joke lambda

--- lib/norris_service.ts
+++ lib/norris_service.ts
@@ -2,16 +2,30 @@ import { Construct } from "constructs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
-
 export class NorrisService extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
+        const layerAxios = new lambda.LayerVersion(this, "AxiosLayer", {
+            code: lambda.Code.fromAsset("resources/layers/axios"),
+            compatibleRuntimes: [lambda.Runtime.NODEJS_18_X]
+        });
+
+        const layerSlackUtils = new lambda.LayerVersion(this, "SlackLayer", {
+            code: lambda.Code.fromAsset("resources/layers/slack-utils"),
+            compatibleRuntimes: [lambda.Runtime.NODEJS_18_X]
+        });
 
         const handler = new lambda.Function(this, "NorrisHandler", {
             runtime: lambda.Runtime.NODEJS_18_X,
             code: lambda.Code.fromAsset("resources"),
             handler: "norris.handler",
+            environment: {
+                API_URL: "https://api.chucknorris.io/jokes/random"
+            },
+            layers: [
+                layerAxios, layerSlackUtils
+            ],
         });
 
         const api = new apigateway.RestApi(this, "norris-jokes-api", {
---
+++ resources/layers/axios/nodejs/axios-utils.ts
@@ -0,0 +1,1 @@
+export * as Axios from 'axios';
